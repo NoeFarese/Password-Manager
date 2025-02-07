93d79a3e6013d24946542f3bab5778fb99048d6a
Noé <noefarese@gmx.ch>
2025-02-07 14:43:45 +0100

    add registration-page template

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  imports: [],
+  templateUrl: './registration.component.html',
+  standalone: true,
+  styleUrl: './registration.component.css'
+})
+export class RegistrationComponent {
+
+}
