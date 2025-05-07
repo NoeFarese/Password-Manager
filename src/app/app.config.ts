ad3e4b8ad478afd6f17ae206cb1d5be34886c9bf
Noé <110814367+NoeFarese@users.noreply.github.com>
2025-05-07 08:42:41 +0200

    add .env file for firebaseConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,10 @@ import {provideRouter} from '@angular/router';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from '@angular/common/http';
 import {routes} from './app.routes';
-import {environment} from './environments';
 import {AngularFireModule} from '@angular/fire/compat';
 import {AngularFireDatabase} from '@angular/fire/compat/database';
 import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {environment} from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
   providers: [
